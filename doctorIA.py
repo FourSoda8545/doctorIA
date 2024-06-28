34473e39c81c1030621e7d39dc4a64fa7b1ec728
FourSoda8545 <aldost939@gmail.com>
2024-06-28 11:06:28 -0600

    versión 3.0 estable

--- doctorIA.py
+++ doctorIA.py
@@ -9,6 +9,9 @@ diagnoses = {
     "fiebre": "Puede ser una infección viral o bacteriana. Descansa y toma muchos líquidos.",
     "tos": "Podría ser un resfriado común o una infección respiratoria. Si persiste, consulta a un médico.",
     "dolor de cabeza": "Podría ser una migraña o tensión. Descansa y mantente hidratado.",
+    "dolor de garganta": "Puede ser una posible irritación de garganta o infección auricular. Toma un desinflamatorio como Dualgos cada 8 horas y descanso de 1 día",
+    "me duele el pene": "Posiblemtente tengas sida, podrías considerar cortartelo hijo de tu puta madre",
+    
     # Añadir más diagnósticos aquí
 }
 
@@ -21,11 +24,14 @@ def home():
 # Definimos la ruta para manejar el formulario de diagnóstico
 @app.route('/diagnose', methods=['POST'])
 def diagnose():
+    
     # Obtenemos los síntomas y alergias del formulario enviado por el usuario
     symptoms = request.form.get('symptoms').lower()
     allergies = request.form.get('allergies').lower()
+    
     # Buscamos el diagnóstico correspondiente en el diccionario
     diagnosis = diagnoses.get(symptoms, "No se encontró un diagnóstico para los síntomas proporcionados.")
+    
     # Renderizamos el archivo HTML 'index.html' con el diagnóstico incluido
     return render_template('index.html', diagnosis=diagnosis)
 
